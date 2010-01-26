5ad57ace255569dc766e679fb17d357b8c6c6689
Ruslan Kornev <oganer@gmail.com>
2010-01-27 00:00:12 +0300

    Подправлен файл локализации, добавлена правильная проверка (с scope) униальности форматов бумаги

--- app/models/paper_format.rb
+++ app/models/paper_format.rb
@@ -1,4 +1,6 @@
 class PaperFormat < ActiveRecord::Base
   validates_numericality_of :width
   validates_numericality_of :height
+  validates_uniqueness_of :width, :scope => :height
+  validates_uniqueness_of :height, :scope => :width
 end
